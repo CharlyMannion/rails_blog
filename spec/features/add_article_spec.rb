dafdab7bf63a4a0a3aff69462a9debba455f866b
Charlotte Mannion <charlottemannion100@gmail.com>
2020-03-11 11:59:00 +0000

    feature tests creation of an article

--- spec/features/add_article_spec.rb
+++ spec/features/add_article_spec.rb
@@ -7,4 +7,14 @@ RSpec.feature "Add an article blog", type: :feature do
     click_link "New article"
     expect(page).to have_content("New Article")
   end
+
+  scenario "User adds article content" do
+    visit "/"
+    click_link "My Blog"
+    click_link "New article"
+    fill_in 'article_title', with: "Test Title"
+    fill_in 'article_text', with: "Test Text"
+    click_button 'Create Article'
+    expect(page).to have_content('Test Title')
+  end
 end
