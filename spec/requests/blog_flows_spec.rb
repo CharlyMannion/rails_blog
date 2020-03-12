8eee4e8b5ad33a9b5ccea26e926309dda6fadbfc
Charlotte Mannion <charlottemannion100@gmail.com>
2020-03-12 09:59:14 +0000

    writes first integration test! Success

--- spec/requests/blog_flows_spec.rb
+++ spec/requests/blog_flows_spec.rb
@@ -1,17 +1,8 @@
-# require 'rails_helper'
-#
-# class BlogFlowTest < ActionDispatch::IntegrationTest
-#   test "can see the welcome page" do
-#     get "/"
-#     assert_select "h1", "Welcome#index"
-#   end
-# end
-#
-# RSpec.describe "BlogFlows", type: :request do
-#   describe "GET /blog_flows" do
-#     it "works! (now write some real specs)" do
-#       get blog_flows_path
-#       expect(response).to have_http_status(200)
-#     end
-#   end
-# end
+require './test/test_helper.rb'
+
+class BlogFlowTest < ActionDispatch::IntegrationTest
+  test "can see the welcome page" do
+    get "/"
+    assert_select "h1", "Hello, rails"
+  end
+end
