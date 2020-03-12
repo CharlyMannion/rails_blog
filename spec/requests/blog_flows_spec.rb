ce28e9b78b6b60cd7a23642f24f1e8a7c992e6f7
Charlotte Mannion <charlottemannion100@gmail.com>
2020-03-12 14:51:08 +0000

    writes further tests for articles controller

--- spec/requests/blog_flows_spec.rb
+++ spec/requests/blog_flows_spec.rb
@@ -5,4 +5,17 @@ class BlogFlowTest < ActionDispatch::IntegrationTest
     get "/"
     assert_select "h1", "Hello, rails"
   end
+
+
+test "can create an article" do
+  get "/articles/new"
+  assert_response :success
+
+  post "/articles",
+    params: { article: { title: "can create", body: "article successfully." } }
+  assert_response :redirect
+  follow_redirect!
+  assert_response :success
+  assert_select "p", "Title:\n  can create"
+end
 end
