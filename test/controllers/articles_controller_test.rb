a5554724fbbc69c6c1c5f841ca5e4f1ad49c503d
Charlotte Mannion <charlottemannion100@gmail.com>
2020-03-12 15:01:54 +0000

    adds controller tests to destroy and edit an article

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -23,4 +23,24 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     get article_url(article)
     assert_response :success
   end
+
+  test "should destroy article" do
+    article = articles(:one)
+    assert_difference('Article.count', -1) do
+      delete article_url(article)
+    end
+
+    assert_redirected_to articles_path
+  end
+
+  test "should update article" do
+    article = articles(:one)
+
+    patch article_url(article), params: { article: { title: "updated" } }
+
+    assert_redirected_to article_path(article)
+    # Reload association to fetch updated data and assert that title is updated.
+    article.reload
+    assert_equal "updated", article.title
+  end
 end
